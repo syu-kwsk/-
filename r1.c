42ea5e1c1b3890704aae6c8fb8aa0cb571c4af56
syu-kwsk <kawasaki.wataru688@mail.kyutech.jp>
2019-08-01 17:42:18 +0900

    Add r1.c void hello_p(void)

---
+++ r1.c
@@ -0,0 +1,19 @@
+/*submit your answer to 1
+"hello, robocar!" をプリントする関数 void hello_p(void).*/
+#include <stdio.h>
+
+void hello_p(void);
+
+int main(void){
+
+  hello_p();
+  
+  return 0;
+}
+
+
+void hello_p(void){
+  
+  printf("hello, robocar!\r\n");
+  
+}
